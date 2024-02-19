11532be4a0d5a0aef263cfc7bdd56642f51d4b59
MatveiKylosov <matveikyl@mail.ru>
2024-02-19 13:27:55 +0500

    Элемент клуб

---
+++ Element/СomputerСlubElement.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practical_lesson_No._28.Element
+{
+    /// <summary>
+    /// Логика взаимодействия для СomputerСlubElement.xaml
+    /// </summary>
+    public partial class СomputerСlubElement : UserControl
+    {
+        public СomputerСlubElement()
+        {
+            InitializeComponent();
+        }
+    }
+}
